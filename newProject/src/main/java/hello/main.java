457bd88bfbc7ad24eff5b134b70441d9f9d2bac9
Kendall Spackman <kendallspackman@ldschurch.org>
2015-05-04 08:25:45 -0600

    Hello world class.

---
+++ newProject/src/main/java/hello/main.java
@@ -0,0 +1,11 @@
+package hello;
+
+/**
+ * Created by kendall on 5/4/15.
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!! :)");
+    }
+}
