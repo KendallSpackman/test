32695e4850495d54135b18205aa11677193d73af
Kendall Spackman <kendallspackman@ldschurch.org>
2015-05-04 08:39:19 -0600

    Another line again.

--- newProject/src/main/java/hello/main.java
+++ newProject/src/main/java/hello/main.java
@@ -8,5 +8,7 @@ public class main {
     public static void main(String[] args) {
         System.out.println("hello world!! :)");
         System.out.println("another line");
+        System.out.println("another line");
+
     }
 }
